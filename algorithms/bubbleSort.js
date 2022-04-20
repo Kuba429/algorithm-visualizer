ab96a13f631e6938624f329f2dca52f3f7f5a2c3
Kuba429 <jakubzawadzki429@gmail.com>
2022-04-20 16:12:28 +0200

    optimized bubble sort significantly

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -3,16 +3,12 @@ import { colorSortedArray, swap } from "../helpers";
 export const bubbleSort = async (array, renderBars) => {
     const delay = false;
     for (let i = 0; i < array.length; i++) {
-        for (let j = i; j < array.length; j++) {
+        for (let j = array.length - 1; j > i; j--) {
             array[i].note = "bubble-i";
             array[j].note = "bubble-j";
 
-            await renderBars(array, delay);
+            // await renderBars(array, delay);
             if (array[i].number > array[j].number) {
-                //highlight match
-                array[i].note = "bubble-i";
-                array[j].note = "bubble-i";
-                // await renderBars(array, delay);
                 swap(array, i, j);
                 await renderBars(array, delay);
             }
